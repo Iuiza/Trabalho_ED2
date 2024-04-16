5d81fcced11c563be00543b5b58c4d59b7ee3a83
Iuiza <luiza.floquinho@gmail.com>
2024-04-16 10:59:35 -0300

    mudanças

--- main.py
+++ main.py
@@ -8,13 +8,22 @@ def home():
     
 @app.route('/etapa1')
 def sobre1():
-    titulo= "Catalogo"
-    catalogo = [
+    titulo = "Produtos"
+    produtos = [
         {"nome": "Cadeira", "quantidade": 8},
         {"nome": "Cafeteira", "quantidade": 4},
         {"nome": "Mixer", "quantidade": 6},
+        {"nome": "Cadeira", "quantidade": 8},
+        {"nome": "Cafeteira", "quantidade": 4},
+        {"nome": "Mixer", "quantidade": 6},
+        {"nome": "Cadeira", "quantidade": 8},
+        {"nome": "Cafeteira", "quantidade": 4},
+        {"nome": "Mixer", "quantidade": 6},
+        {"nome": "Cadeira", "quantidade": 8},
+        {"nome": "Cafeteira", "quantidade": 4},
+        {"nome": "Mixer", "quantidade": 6}
     ]
-    return render_template('etapa1.html', titulo = titulo, catalogo = catalogo)
+    return render_template('etapa1.html', titulo = titulo, produtos = produtos)
     
 if __name__ == "__main__":
     app.run(debug=True)
