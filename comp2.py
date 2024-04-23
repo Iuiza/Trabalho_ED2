849db86bd770a3a4da64b103e40a4692e65c7521
Luiza <84250561+Iuiza@users.noreply.github.com>
2024-04-23 16:21:36 -0300

    comp certo

--- comp2.py
+++ comp2.py
@@ -2,9 +2,9 @@ import gzip
 
 # # compactação
 # # bom para textos  (algoritmo sem perda) (.txt, .json, .csv e etc)
-# with open('trabalho.pdf', 'rb') as arquivo_original:
-#     with gzip.open('trabalho.pdf.gz', 'wb') as arquivo_compactado:
-#         arquivo_compactado.write(arquivo_original.read())
+with open('trabalho.pdf', 'rb') as arquivo_original:
+    with gzip.open('trabalho.pdf.gz', 'wb') as arquivo_compactado:
+         arquivo_compactado.write(arquivo_original.read())
 
 # # descompactação
 # with gzip.open('trabalho.pdf.gz', 'rb') as file:
@@ -14,16 +14,16 @@ import gzip
 #     file.write(file_content)
 
 # varios arquivos de uma pasta
-import os
+# import os
 
-folder_path = './' # pasta que os arquivos serão compactados
+# folder_path = './' # pasta que os arquivos serão compactados
 
-for file_name in os.listdir(folder_path):
-    # os.path.join(folder_path, file_name) == (folder_path + '/' + file_name)  # compara se é um arquivo
-    if os.path.isfile(os.path.join(folder_path, file_name)):
-        with open(os.path.join(folder_path, file_name), 'rb') as file_in:
-            with gzip.open(os.path.join(folder_path, f'{file_name}.gz'), 'wb') as file_out:
-                file_out.writelines(file_in)
+# for file_name in os.listdir(folder_path):
+#     # os.path.join(folder_path, file_name) == (folder_path + '/' + file_name)  # compara se é um arquivo
+#     if os.path.isfile(os.path.join(folder_path, file_name)):
+#         with open(os.path.join(folder_path, file_name), 'rb') as file_in:
+#             with gzip.open(os.path.join(folder_path, f'{file_name}.gz'), 'wb') as file_out:
+#                 file_out.writelines(file_in)
 
 
 
