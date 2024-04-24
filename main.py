678478c2bffdb852f32389012675dc6bfb800e86
Iuiza <luiza.floquinho@gmail.com>
2024-04-24 17:35:09 -0300

    add download

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, send_file
+import gzip
 
 app = Flask(__name__)
 
@@ -10,22 +11,18 @@ def home():
 def sobre1():
     titulo= "Catalogo"
     import json
-
     # Abra o arquivo JSON
     with open('produtos.json', 'r') as f:
         # Carregue o conteúdo do arquivo JSON como um dicionário
         catalogo = json.load(f)
-
-    # Agora 'data' é um dicionário contendo o conteúdo do arquivo JSON
-    # print(catalogo)
-
-
-    # catalogo = [
-    #     {"nome": "Cadeira", "quantidade": 8},
-    #     {"nome": "Cafeteira", "quantidade": 4},
-    #     {"nome": "Mixer", "quantidade": 6},
-    # ]
     return render_template('etapa1.html', titulo = titulo, catalogo = catalogo)
+
+@app.route('/download')
+def download_file():
+    with open('produtos.json', 'rb') as arquivo_original:
+        with gzip.open('produtos.json.gz', 'wb') as arquivo_compactado:
+            arquivo_compactado.write(arquivo_original.read())
+    return send_file('produtos.json.gz', as_attachment=True)
     
 if __name__ == "__main__":
     app.run(debug=True)
